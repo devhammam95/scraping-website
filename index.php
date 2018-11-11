caf2ef78c576d50235f2cc0112f4480c4ed798bb
mohamed <you@example.com>
2018-11-11 04:14:09 +0200

    edit in variables

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
 echo "<center>Start Scraping thescienceexplorer<center>"."<br/>";
 include 'simple_html_dom.php';
+$urlFormat  = 'http://thescienceexplorer.com/categories/technology?page=';
+$data       = array();
+
 for($i=0;$i<=85;$i++){
     echo "Start Scraping page number - ".($i+1)."<br/>";
-    $url  = 'http://thescienceexplorer.com/categories/technology?page='.$i;
+    $url  = $urlFormat.$i; //echo $url;
     get_content($url);
     echo "End Scraping page number - ".($i+1)."<br/>";
 }
 echo "End Scraping thescienceexplorer"."<br/>";
 // function of scraping
 function get_content($url){
-    $data = array();
     $html = str_get_html(file_get_contents($url));
-//$rows = $html->find("[@id=featured_shorts]/li");
     $rows = $html->find("//div[@class=teaser-list]/article/header/h1");
     if (!count($rows)){
         echo"---- No data found ----"."<br>";
@@ -31,6 +32,5 @@ function get_content($url){
   //  print_r($data);
 
 }
-
 
 ?>
